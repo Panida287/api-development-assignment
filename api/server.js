f6836ad3b147229bc95b2e4fcef7186c916d679f
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2025-01-19 19:33:30 +0100

    feat: refactor server.js for Vercel deployment

--- api/server.js
+++ api/server.js
@@ -16,6 +16,5 @@ app.get('/', (req, res) => {
     res.send({ message: 'MongoDB is connected!' });
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+// Export the app for Vercel
+module.exports = app;
