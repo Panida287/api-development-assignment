d9e45ec804253af54afdd470c82528ff76a8f25a
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2025-01-26 18:40:11 +0100

    feat: added login button

--- frontend/src/js/api/constants.js
+++ frontend/src/js/api/constants.js
@@ -1,7 +1,6 @@
-export const API_BASE = 'https://api.panidapaethanom.com';
+export const API_BASE = 'https://api-development-assignment-git-main-panida-paethanoms-projects.vercel.app';
 export const API_LOGIN = `${API_BASE}/auth/login`;
 export const API_REGISTER = `${API_BASE}/auth/signup`;
-export const REFRESH_TOKEN = `${API_BASE}/auth/refresh`;
 export const API_MOVIES= `${API_BASE}/movies`;
 export const API_CATEGORIES= `${API_BASE}/categories`;
 export const API_DIRECTORS= `${API_BASE}/directors`;
